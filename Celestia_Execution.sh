393c6c2593346f7d3a328221937d2d39ffc3f9b2
byonjuk <pleasediechinese@gmail.com>
2024-09-07 16:57:50 +0900

    셀레 2번(다시 올림)

--- Celestia_Execution.sh
+++ Celestia_Execution.sh
@@ -154,7 +154,7 @@ sleep 10
 print_color "36" "상태창 띄워드릴게용~"
 sudo systemctl status celestia-light --no-pager
 
-print_color "35" "이후 'screen -r Celestia' 쳐서 'sudo journalctl -u celestia-light -f' 입력 후에 로그가 제대로 나오는지 확인"
+print_color "35" "이후 'screen -S Celestia_logs' 쳐서 'sudo journalctl -u celestia-light -f' 입력 후에 로그가 제대로 나오는지 확인"
 print_color "34" "로그가 주르륵 올라오면 컨트롤 A + D 를 눌러서 스크린에서 탈출해 보세요"
 
 exec bash
