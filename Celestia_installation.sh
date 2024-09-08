313179cab55afec1184d5813465034d01beb5d4e
byonjuk <pleasediechinese@gmail.com>
2024-09-08 15:29:39 +0900

    셀레스티아 1번

--- Celestia_installation.sh
+++ Celestia_installation.sh
@@ -61,12 +61,14 @@ sudo apt-get update
 
 # updating go
 print_color "33" "installing golang-go ..."
-sudo apt remove golang-go -y
-sudo apt autoremove -y
-wget https://go.dev/dl/go1.23.0.linux-amd64.tar.gz
+ver="1.23.0"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
-rm go1.23.0.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
 
 print_color "32" "source $HOME/.bash_profile"
 source $HOME/.bash_profile
